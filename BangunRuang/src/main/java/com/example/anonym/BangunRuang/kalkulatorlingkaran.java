09a0579bbc5f17fca33a3bdc7bb4d51c36f1cf32
yusil86 <pendudukfilm@gmail.com>
2018-12-21 00:01:25 +0800

    project pertama

--- BangunRuang/src/main/java/com/example/anonym/BangunRuang/kalkulatorlingkaran.java
+++ BangunRuang/src/main/java/com/example/anonym/BangunRuang/kalkulatorlingkaran.java
@@ -9,32 +9,54 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class kalkulatorlingkaran extends AppCompatActivity {
-EditText edtjarijari;
-Button btnhitung;
-TextView tvhasil;
-double hasil;
+    EditText edtjarijari, edtjarijari2,edtdiameter;
+    Button btnhitung, btnhitung2;
+    TextView tvhasil, tvhasil2;
+    double hasil, hasil2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalkulatorlingkaran);
-    edtjarijari = (EditText) findViewById(R.id.edt_jarijari);
-    btnhitung = (Button)findViewById(R.id.btn_hitunglingkaran);
-    tvhasil = (TextView) findViewById(R.id.tv_hasillingkaran);
- btnhitung.setOnClickListener(new View.OnClickListener() {
-     @Override
-     public void onClick(View v) {
-         boolean isikolom = false;
-         if (TextUtils.isEmpty(edtjarijari.getText().toString())){
-             isikolom = true ;
-             edtjarijari.setError("Kolom tidak boleh kosong");
-         }
-         if (!isikolom){
-             hasil = Double.parseDouble(edtjarijari.getText().toString()) *
-                     Double.parseDouble(edtjarijari.getText().toString()) * 3.14 ;
-             tvhasil.setText("Luas lingkaran adalah =" + hasil+ "m2");
-         }
-     }
- });
+        edtjarijari = (EditText) findViewById(R.id.edt_jarijari);
+        edtjarijari2 = (EditText) findViewById(R.id.edt_jarijari2);
+        btnhitung = (Button) findViewById(R.id.btn_hitunglingkaran);
+        btnhitung2 = (Button) findViewById(R.id.btn_hitungkelilinglingkaran);
+        tvhasil = (TextView) findViewById(R.id.tv_hasillingkaran);
+        tvhasil2 = (TextView) findViewById(R.id.tv_hasilkelilinglingkaran);
+        btnhitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isikolom = false;
+                if (TextUtils.isEmpty(edtjarijari.getText().toString())) {
+                    isikolom = true;
+                    edtjarijari.setError("Kolom tidak boleh kosong");
+
+                }
+                if (!isikolom) {
+                    hasil = Double.parseDouble(edtjarijari.getText().toString()) *
+                            Double.parseDouble(edtjarijari.getText().toString()) * 3.14;
+                    tvhasil.setText("Luas lingkaran adalah =" + hasil + "cm2");
+                }
+            }
+        });
+        btnhitung2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isikolom = false;
+                if (TextUtils.isEmpty(edtjarijari2.getText().toString())) {
+                    isikolom = true;
+                    edtjarijari2.setError("Kolom tidak boleh kosong");
+
+
+                }
+                if (!isikolom) {
+                    hasil2 = Double.parseDouble(edtjarijari2.getText().toString()) * 2 * 3.14;
+                    tvhasil2.setText("Keliling lingkaran adalah =" + hasil2+"cm");
+                }
+
+            }
+        });
     }
 
 }
